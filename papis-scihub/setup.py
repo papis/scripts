a61366fd1b692b67b04c4f499b19df157e7feec7
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-21 12:00:33 +0200

    scihub: Bump version

--- papis-scihub/setup.py
+++ papis-scihub/setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as fd:
 
 setup(
     name='papis-scihub',
-    version='1.3.0',
+    version='1.4.0',
     author='Alejandro Gallo',
     author_email='aamsgallo@gmail.com',
     license='GPLv3',
