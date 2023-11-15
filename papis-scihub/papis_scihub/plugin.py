cf099a3dca4ded2369f03282c90fb35dc79b805f
gesh <gesh@gesh.uni.cx>
2023-11-15 23:48:13 +0200

    Fix build, lint
    * Restore static versions (at least until repo starts tagging versions)
      * Add (commented-out) [tool.setuptools_scm] section to silence
        warnings once repo uses dynamic versioning
    * Restore unnecessary edits
      * Restore liability warning
      * Restore version bounds (guessed for bs4)
      * Restore classifiers, keywords, authors
      * Relax python version bounds
      * Explicitly use standard GPL3 text
    * Make pylint-clean for python >=3.6
      * Convert `| None` to uses of `Optional[]` to silence warnings on
        Pythons older than 3.10
      * Use more specific exceptions
      * Clean up early-exit logic, drop useless exception catches
      * Standardize names
    * Misc improvements
      * Fix `download_bibtex()` type
      * Add sci-hub mirrors

--- papis-scihub/papis_scihub/plugin.py
+++ papis-scihub/papis_scihub/plugin.py
@@ -1,36 +1,56 @@
 from urllib.parse import urlparse
+from typing import Optional
+from requests.exceptions import RequestException
 
 import doi
 import papis.downloaders
 from bs4 import BeautifulSoup
 
+import colorama
 
-BASE_URLS = ("http://sci-hub.ee",)
+WARNING_NOTICE = '''
+{bb} .+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+. {ns}
+{bb}(                                                                 ){ns}
+{bb} )                    {rb} WARNING NOTICE {bb}                           \
+( {ns}
+{bb}(                     ----------------                            ){ns}
+{bb} )                                                               ( {ns}
+{bb}(  This script uses the platform {rb}SCIHUB{bb}, which may or MAY NOT     \
+){ns}
+{bb} ) be in conflict with local laws in your country. Use it at     ( {ns}
+{bb}(  your own risk, {rb}the author bears no responsibility{bb}.             \
+){ns}
+{bb} )                                                               ( {ns}
+{bb}(                                           papis team            ){ns}
+{bb} "+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+"+.+" {ns}\
+'''.format(
+    bb=colorama.Back.BLACK + colorama.Fore.WHITE,
+    ns=colorama.Style.RESET_ALL,
+    rb=colorama.Back.RED,
+)
 
+BASE_URLS = (f"http://sci-hub.{tld}" for tld in ["ee","se","st","ru"])
 
 class Downloader(papis.downloaders.Downloader):
     def __init__(self, uri: str) -> None:
+        self.logger.warning(WARNING_NOTICE)
         papis.downloaders.Downloader.__init__(self, uri=uri, name="sci-hub")
         self.expected_document_extension = "pdf"
         self.priority = 1
-
-        try:
-            self._get_active_server_url()
-        except Exception as e:
-            raise e
-
+        self._get_active_server_url()
         self.doi = _extract_doi(uri)
         self._body = self.session.get(
             f"{self.base_url}/{self.doi}",
             verify=False
         )
+        self.bibtex_data = ""
 
     @classmethod
-    def match(cls, url: str) -> papis.downloaders.Downloader | None:
+    def match(cls, url: str) -> Optional[papis.downloaders.Downloader]:
         try:
             _extract_doi(url)
             return Downloader(url)
-        except Exception:
+        except (RequestException, ValueError):
             return None
 
     def _get_active_server_url(self) -> None:
@@ -38,35 +58,36 @@ class Downloader(papis.downloaders.Downloader):
             if self._ping_server(base_url):
                 self.base_url = base_url
                 return
-        raise Exception("No Sci-Hub servers can be pinged")
+        raise RequestException("No Sci-Hub servers can be pinged")
 
     def _ping_server(self, base_url: str) -> bool:
         try:
             ping = self.session.get(base_url, timeout=1, verify=False)
-            if ping.status_code != 200:
-                self.logger.error(f"server {base_url} is down")
-                return False
-            else:
-                self.logger.debug(f"server {base_url} is up")
-                return True
-        except Exception:
+        except RequestException:
             return False
 
-    def get_doi(self) -> str | None:
+        if ping.status_code != 200:
+            self.logger.error(f"server {base_url} is down")
+            return False
+
+        self.logger.debug(f"server {base_url} is up")
+        return True
+
+    def get_doi(self) -> Optional[str]:
         return self.doi
 
-    def get_document_url(self) -> str | None:
-        s = BeautifulSoup(self._body.content, "html.parser")
-        iframe = s.find("iframe")
-        if iframe:
-            src = iframe.get("src")
-            if src.startswith("//"):
-                src = f"https:{src}"
-            return src
-        else:
+    def get_document_url(self) -> Optional[str]:
+        soup = BeautifulSoup(self._body.content, "html.parser")
+        iframe = soup.find("iframe")
+        if not iframe:
             return None
 
-    def download_bibtex(self) -> str:
+        src = iframe.get("src")
+        if src.startswith("//"):
+            src = f"https:{src}"
+        return src
+
+    def download_bibtex(self) -> None:
         self.bibtex_data = self.session.get(
             f"https://doi.org/{self.doi}",
             headers={"accept": "application/x-bibtex"}
@@ -83,6 +104,6 @@ def _extract_doi(url: str) -> str:
     try:
         doi.validate_doi(doi_)
         return doi_
-    except Exception as e:
-        raise e(f"Cannot extract a valid DOI from the provided URL: {url}")
-
+    except ValueError as err:
+        raise ValueError(
+            f"Cannot extract a valid DOI from the provided URL: {url}") from err
